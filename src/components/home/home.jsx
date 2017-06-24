b2c0e20c3a7b9eba91c6d4f2ab005a65efbe1594
Miracle Everett <miracle.everett@gmail.com>
2017-06-23 20:07:24 -0700

    first commit

---
+++ src/components/home/home.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import CSSModules from 'react-css-modules';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div styleName="wrapper">
+        <header>
+          <ul>
+            <li>
+              How to Play
+            </li>
+            <li>
+              Rules
+            </li>
+            <li>
+              About Us
+            </li>
+            <li>
+              Contact
+            </li>
+          </ul>
+        </header>
+          <div styleName="hey-turkey">
+            <div styleName="hey-turkey-bird">
+
+            </div>
+            <div styleName="hey-turkey-speech">
+
+            </div>
+            <div styleName="hey-turkey-teams">
+
+            </div>
+            <div styleName="hey-turkey-choose">
+              <div styleName="hey-turkey-choose-red">
+
+              </div>
+              <div styleName="hey-turkey-choose-blue">
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+    );
+  }
+}
